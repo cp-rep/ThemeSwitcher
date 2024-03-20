fdcb1efd63b91aa9098f6051bcba5d61f5c323af
cp <ctpanning@gmail.com>
2024-03-19 17:30:26 -0700

    updates to const values

--- lib/_cursesWinConsts.hpp
+++ lib/_cursesWinConsts.hpp
@@ -33,42 +33,43 @@ enum _WINS {
   _TEXTWIN
 };
 
-// _HELPWIN DIMENSION CONSTS
 const unsigned int _HELPWINSTARTY = 2;
-const unsigned int _HELPWINSTARTCOLS = 60;
-const unsigned int _HELPWINMINLINES = 10;
-const unsigned int _HELPWINMINCOLS = 40;
+const unsigned int _HELPWINSTARTX = 0;
+const unsigned int _HELPWINMINCOLS = 50;
+const unsigned int _HELPWINMAXCOLS = 60;
+const unsigned int _HELPWINMINLINES = 20;
+const unsigned int _HELPWINMAXLINES = 40;
 
-// _PROGRAMSWIN DIMENSION CONSTS
+// _PROGRAMSWIN DIMENSION  S
 const unsigned int _PROGRAMSWINSTARTCOLS = 60;//
 const unsigned int _PROGRAMSWINMINLINES = 10;//
 const unsigned int _PROGRAMSWINMINCOLS = 40;
 
-// _PROMPTWIN DIMENSION CONSTS
+// _PROMPTWIN DIMENSION  S
 const unsigned int _PROMPTWINSTARTY = 2;
 const unsigned int _PROMPTWINSTARTX = 3;
 const unsigned int _PROMPTWINMINLINES = 6;
 const unsigned int _PROMPTWINMAXLINES = 6;
 const unsigned int _PROMPTWINMINCOLS = 60;
-const unsigned int _PROMPTWINMAXCOLS = 120;
+const unsigned int _PROMPTWINMAXCOLS = 140;
 
-// _SAVEDFILESWIN DIMENSION CONSTS
+// _SAVEDFILESWIN DIMENSION  S
 const unsigned int _SAVEDFILESWINSTARTY = _PROMPTWINSTARTY + _PROMPTWINMAXLINES + 1;
 const unsigned int _SAVEDFILESWINSTARTX = 3;
 const unsigned int _SAVEDFILESWINMINLINES = 16;
 const unsigned int _SAVEDFILESWINMAXLINES = 24;
 const unsigned int _SAVEDFILESWINMINCOLS = 60;
-const unsigned int _SAVEDFILESWINMAXCOLS = 120;
+const unsigned int _SAVEDFILESWINMAXCOLS = 140;
 
-// _SAVEDFILESWIN DIMENSION CONSTS
+// _SAVEDFILESWIN DIMENSION  S
 const unsigned int _SAVEDTHEMESWINSTARTY = _PROMPTWINSTARTY + _PROMPTWINMAXLINES + 1;
 const unsigned int _SAVEDTHEMESWINSTARTX = 3;
 const unsigned int _SAVEDTHEMESWINMINLINES = 16;
 const unsigned int _SAVEDTHEMESWINMAXLINES = 24;
 const unsigned int _SAVEDTHEMESWINMINCOLS = 60;
-const unsigned int _SAVEDTHEMESWINMAXCOLS = 120;
+const unsigned int _SAVEDTHEMESWINMAXCOLS = 140;
 
-// _TEXTWIN DIMENSION CONSTS
+// _TEXTWIN DIMENSION  S
 const unsigned int _TEXTWINSTARTY = 0;
 const unsigned int _TEXTWINSTARTX = 0;
 const unsigned int _TEXTWINLINES = 1;
