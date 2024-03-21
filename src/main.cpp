552f1f0dbddd9fb7d1a27bf48812ba05e3c2a2e2
cp <ctpanning@gmail.com>
2024-03-21 10:14:36 -0700

    moved the string testing functions to their own module in testingInterface.hpp/.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -28,18 +28,12 @@
 #include "cursesFunctions.hpp"
 #include "cursesWindow.hpp"
 #include "log.hpp"
+#include "testingInterface.hpp"
 
 #define _DEBUG 0
 #define _LOG 1
 #define _CURSES 1
 
-
-void initTestStringVector(std::vector<std::string>& testStrings,
-                          const int numStrings,
-                          const int numChars,
-                          std::ofstream& log);
-void printTestStringVectorToLog(const std::vector<std::string>& strings,
-                                std::ofstream& log);
 
 
 // ==== main ==================================================================
@@ -178,58 +172,3 @@ int main()
 
   return 0;
 } // end of "main"
-
-
-
-void initTestStringVector(std::vector<std::string>& testStrings,
-                          const int numStrings,
-                          const int numChars,
-                          std::ofstream& log)
-{
-  std::string tempString;
-  bool adjusted = false;
-  std::string extension = ".ext";
-
-  for(int i = 0; i < numStrings; i++)
-  {
-    char temp = 'a';
-    if(i < 9)
-      {
-        tempString.append(" ");
-      }
-    for(int j = 0; j < numChars; j++)
-      {
-        if(j >= numChars - extension.length())
-          {
-            tempString.append(extension);
-          }
-        else
-          {
-            tempString.push_back(temp);
-            temp++;
-            if(temp == 'z')
-              {
-                temp = 'a';
-              }
-          }
-      }
-
-    testStrings.push_back(tempString);
-    tempString.clear();
-  }
-} // end of "initTestStringVector"
-
-
-
-void printTestStringVectorToLog(const std::vector<std::string>& strings,
-                                std::ofstream& log)
-{
-  std::vector<std::string>::const_iterator it;
-  int i = 0;
-
-  for(it = strings.begin(); it != strings.end(); it++)
-    {
-      log << i << ": " << *it << std::endl;
-      i++;
-    }
-} // end of "printTestStringVector"
