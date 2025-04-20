a7394e4e9ceea1e074842b72923eb378677ef6d0
cp <ctpanning@gmail.com>
2025-04-20 09:16:17 -0700

    initial commit for the programStates.hpp header file. no code included

---
+++ lib/programStates.hpp
@@ -0,0 +1,9 @@
+#ifndef PROGRAMSTATE_H_
+#define PROGRAMSTATE_H_
+
+// void enterHWSFaddFileState(const std::unordered_map<int, CursesWindow*>& wins,
+//                             const int HWSFWinIndex,
+//                             const int HWSFAddFileWinIndex,
+//                             const int
+
+#endif // PROGRAMSTATE_H_
