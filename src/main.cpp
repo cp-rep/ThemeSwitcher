bbbd6ae19c533b1c9fcbd7a0d13273a3b7ed38f9
cp <ctpanning@gmail.com>
2025-04-20 10:02:37 -0700

    moved state logic for _HWSFADDFILE state to programStates.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -361,50 +361,8 @@ int main()
                            log);
               break;
             case _HWSFADDFILE:
-              // enterHWSFAddFileState(wins,
-              //                       log);
-
-              // flashButton(wins,
-              //             _HWSFADDFILE,
-              //             _hwSFAddFile,
-              //             _BLACK_TEXT,
-              //             _WHITE_TEXT,
-              //             log);
-              // defineSFPromptWin(wins,
-              //                   log);
-              // printPrompt(wins,
-              //             _SFPROMPTWIN,
-              //             _hwSFAddFileWin,
-              //             log);
-
-              // yOffset = 2;
-              // xOffset = 2;
-              // CursesWindow* userInputWindow = new CursesWindow();
-              // wins.insert(std::make_pair(_USERINPUTWIN, userInputWindow));
-              // wins.at(_USERINPUTWIN)->defineWindow(newwin(1,
-              //                                             wins.at(_SFPROMPTWIN)->getNumCols(),
-              //                                             0,
-              //                                             0),
-              //                                      "UserInputWindow",
-              //                                      0,
-              //                                      wins.at(_SFPROMPTWIN)->getStartY(),
-              //                                      wins.at(_SFPROMPTWIN)->getStartX(),
-              //                                      10);
-
-              // while(input != KEY_ENTER)
-              //   {
-              //     input = getch();
-              //     printUserInput(wins,
-              //                    _SFPROMPTWIN,
-              //                    input,
-              //                    yOffset,
-              //                    xOffset);
-              //     flushinp();
-              //     refreshwins(wins);
-              //     doupdate();
-              //     usleep(15000);
-              //   }
-              usleep(1000000);
+              enterHWSFAddFileState(wins,
+                                    log);
               break;
             case _HWSFEDITFILEPATH:
               flashButton(wins,
