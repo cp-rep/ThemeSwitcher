fa8a520f169c63b61b7401843fb80886878b79d7
cp <ctpanning@gmail.com>
2025-04-20 09:15:32 -0700

    initial commit for the const strings used for various ThemeSwitcher Ncurses windows

---
+++ lib/_winStringConsts.hpp
@@ -0,0 +1,37 @@
+#ifndef _WINSTRINGCONSTS_H_
+#define _WINSTRINGCONSTS_H_
+#include <string>
+
+// helpwin
+const std::string _hwSFAddFile = "     Add File      ";
+const std::string _hwSFEditFilePath = "   Edit File Path  ";
+const std::string _hwSFViewFilePath = "   View File Path  ";
+const std::string _hwSFRemoveFile = "    Remove File    ";
+const std::string _hwSFAddTheme = "  Add File Theme   ";
+const std::string _hwSFEditTheme = "  Edit File Theme  ";
+const std::string _hwSFRemoveTheme = " Remove File Theme ";
+const std::string _hwSTAddTheme = "     Add Theme     ";
+const std::string _hwSTARemoveTheme = "     Add Theme     ";
+const std::string _hwSTRemoveTheme = "   Remove Theme    ";
+const std::string _hwSTEditTheme =   "     Edit Theme    ";
+const std::string _hwSTViewTheme = "    View Theme     ";
+
+// other prompts
+const std::string _hwSFAddFileWin = "Enter Full Path To File: ";
+
+// _SAVEDFILESWIN
+const std::string sfTitle = "SAVED FILES:";
+const std::string sfThemeTitle = "CURRENT THEME:";
+const std::string sfLeftArrow = "_LARROWSAVEDFILESWIN";
+const std::string sfRightArrow = "_RARROWSAVEDFILESWIN";
+
+// _SAVEDTHEMESWIN
+const std::string stTitle = "SAVED THEMES:";
+const std::string stLeftArrow = "_LARROWSAVEDTHEMESWIN";
+const std::string stRightArrow = "_RARROWSAVEDTHEMESWIN";
+
+// arrows
+const std::string leftArrow = " < ";
+const std::string rightArrow = " > ";
+
+#endif // _WINSTRINGCONSTS_H_
