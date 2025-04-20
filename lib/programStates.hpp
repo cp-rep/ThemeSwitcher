38c17642a53402b9e56ee8e748a06a47ad7e6ef2
cp <ctpanning@gmail.com>
2025-04-20 09:24:27 -0700

    renamed file programState.hpp to programStates.hpp and added necessary includes for future operations

--- lib/programStates.hpp
+++ lib/programStates.hpp
@@ -1,9 +1,18 @@
-#ifndef PROGRAMSTATE_H_
-#define PROGRAMSTATE_H_
+#ifndef PROGRAMSTATES_H_
+#define PROGRAMSTATES_H_
+#include <string>
+#include "_cursesWinConsts.hpp"
+#include "_progStateConsts.hpp"
+#include "_winStringConsts.hpp"
+#include "cursesFunctions.hpp"
+#include "cursesWindow.hpp"
+#include "fileOperations.hpp"
+#include "log.hpp"
 
 // void enterHWSFaddFileState(const std::unordered_map<int, CursesWindow*>& wins,
-//                             const int HWSFWinIndex,
-//                             const int HWSFAddFileWinIndex,
-//                             const int
+//                            const int HWSFWinIndex,
+//                            const int HWSFAddFileWinIndex,
+//                            const int
+//                            std::ofstream& log);
 
-#endif // PROGRAMSTATE_H_
+#endif // PROGRAMSTATES_H_
