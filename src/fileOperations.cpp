5a8a68387ff15eefc4f09c88136d95ec464547ae
cp <ctpanning@gmail.com>
2025-04-26 19:09:43 -0700

    added function definition for doesDirectoryExist()

--- src/fileOperations.cpp
+++ src/fileOperations.cpp
@@ -1,6 +1,22 @@
 #include "fileOperations.hpp"
 
 
+
+
+bool doesDirectoryExist(const std::string& directoryFullPath)
+{
+  struct stat file;
+
+  if( stat(directoryFullPath.c_str(), &file) )
+    {
+      return false;
+    }
+
+  return ( (file.st_mode & S_IFDIR) != 0 );
+} // end of "doesDirectoryExist"
+
+
+
 int hwSFAddFile(std::vector<std::string> sfStrings)
 {
 
